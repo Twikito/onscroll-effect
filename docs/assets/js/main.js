521084542e8a0d0dd4101fd756202f55cf4355cb
Matthieu Bué <matthieu.bue@gmail.com>
2018-08-30 12:18:04 +0200

    New doc

---
+++ docs/assets/js/main.js
@@ -0,0 +1,39 @@
+(() => {
+
+	const scroll = new SmoothScroll("a[href^='#']:not([role='button'])", {
+		speed: 500,
+		easing: 'easeInOutQuint'
+	});
+
+	const getRandomInt = max => Math.floor(Math.random() * Math.floor(max));
+
+	const container = document.querySelector(".demo");
+
+	for (let i = 0; i <= 97; i++) {
+
+		let btn = document.createElement("button");
+
+		btn.setAttribute("type", "button");
+		btn.setAttribute("data-scroll", "");
+		btn.setAttribute("title", "data-scroll");
+
+		if (getRandomInt(3)) {
+			btn.classList.add("is-round");
+			if (getRandomInt(2)) {
+				btn.setAttribute("data-scroll-repeat", "true");
+				btn.setAttribute("title", `${btn.getAttribute("title")} data-scroll-repeat="true"`);
+			} else {
+				btn.setAttribute("data-scroll-repeat", getRandomInt(9)+1);
+				btn.setAttribute("title", `${btn.getAttribute("title")} data-scroll-repeat="${btn.getAttribute("data-scroll-repeat")}"`);
+			}
+		}
+
+		if (getRandomInt(2)) {
+			btn.setAttribute("data-scroll-offset", getRandomInt(30)*10);
+				btn.setAttribute("title", `${btn.getAttribute("title")} data-scroll-offset="${btn.getAttribute("data-scroll-offset")}"`);
+		}
+
+		container.appendChild(btn);
+	}
+
+})();
