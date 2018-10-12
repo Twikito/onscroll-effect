91359b7465dc8fb81aac5f54216a886d36d67b59
Matthieu Bué <matthieu.bue@gmail.com>
2018-10-12 14:57:27 +0200

    Version update

--- dist/onscroll-effect.es6.js
+++ dist/onscroll-effect.es6.js
@@ -4,7 +4,7 @@
  * A tiny JavaScript library to enable CSS animations when user scrolls.
  *
  * @author Matthieu Bué <https://twikito.com>
- * @version v1.1.1
+ * @version v1.1.2
  * @link https://twikito.github.io/onscroll-effect/
  * @license MIT
  * -------------------------------------------------------------------
